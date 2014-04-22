ca55fe406071dc586b38bf49a0c315a1187098bd
Jen Hamon <jhamon@gmail.com>
2014-04-22 00:09:49 -0700

    Add a bit of color.

--- src/view.js
+++ src/view.js
@@ -1,13 +1,33 @@
 (function () {
   var GOL = window.GOL = (window.GOL || {});
 
-  GOL.numPixels = 5000; // Smaller gives better performance.
+  GOL.numPixels = 4000; // Smaller gives better performance.
 
   var View = GOL.View = function () {
     this.canvas = document.getElementById('mycanvas');
     this.ctx = this.canvas.getContext('2d');
     this.configure();
   }
+
+  View.prototype.palettes = [
+    ['#44A437', '#081017'], // my original
+    ['#081017', '#44A437'], // my original
+    // Nice palettes from colourlovers.com:
+    ['#003A54', '#325D6F'], // Dawn Watch
+    ['#325D6F', '#003A54'], // Dawn Watch
+    ['#FFF700', '#004E72'], // The Last Summer
+    ['#004E72', '#FFF700'], // The Last Summer
+    ['#80B31A', '#082A3A'], // Phone Never Answer
+    ['#082A3A', '#80B31A'], // Phone Never Answer
+    ['#1CB4FA', '#FF0022'], // Friendship
+    ['#FF0022', '#1CB4FA'], // Friendship
+    ['#242004', '#009E77'], // Clementine Gozmin
+    ['#009E77', '#242004'], // Clementine Gozmin
+    ['#FF3434', '#C8D319'], // Sad
+    ['#C8D319', '#FF3434'], // Sad
+    ['#6B07C7', '#D3D0D6'], // Planet Industry
+    ['#D3D0D6', '#6B07C7']  // Planet Industry
+  ];
 
   View.prototype.configure = function () {
     // Resize canvas element to fix screen.
@@ -24,12 +44,16 @@
     // Compute needed data grid sizes
     this.gridHeight = window.innerHeight / this.pixelSize | 0 + 1;
     this.gridWidth = window.innerWidth / this.pixelSize | 0 + 1;
+
+    // Select a palette.
+    this.palette = this.palettes[(Math.random() * this.palettes.length | 0)];
+
   };
 
   View.prototype.drawPixel = function (x, y, value) {
     var pixelX = this.dx * x;
     var pixelY = this.dx * y;
-    this.ctx.fillStyle = value ? "#44A437" : "#081017";
+    this.ctx.fillStyle = value ? this.palette[0] : this.palette[1];
     this.ctx.fillRect(pixelX, pixelY, this.dx, this.dx);
   };
 
@@ -50,6 +74,6 @@
     'command':'init', 
     x: v.gridWidth,
     y: v.gridHeight
-  })
+  });
 
 })(this);
