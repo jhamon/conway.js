404694286ee6aad11a3958d2e6b94d68edbce364
Jen Hamon <jhamon@gmail.com>
2014-04-27 01:40:58 -0700

    Move color palettes to seperate file.

---
+++ src/colors.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+    var LIFE = window.LIFE = (window.LIFE || {});
+
+    LIFE.palettes = [
+        ['#44A437', '#081017'], // my original
+        ['#081017', '#44A437'], // my original
+        // Nice palettes from colourlovers.com:
+        ['#003A54', '#325D6F'], // Dawn Watch
+        ['#325D6F', '#003A54'], // Dawn Watch
+        ['#FFF700', '#004E72'], // The Last Summer
+        ['#004E72', '#FFF700'], // The Last Summer
+        ['#80B31A', '#082A3A'], // Phone Never Answer
+        ['#082A3A', '#80B31A'], // Phone Never Answer
+        ['#1CB4FA', '#FF0022'], // Friendship
+        ['#FF0022', '#1CB4FA'], // Friendship
+        ['#242004', '#009E77'], // Clementine Gozmin
+        ['#009E77', '#242004'], // Clementine Gozmin
+        ['#FF3434', '#C8D319'], // Sad
+        ['#C8D319', '#FF3434'], // Sad
+        ['#6B07C7', '#D3D0D6'], // Planet Industry
+        ['#D3D0D6', '#6B07C7'],  // Planet Industry
+        ['#F2EEB3', '#260126'],  // Vintage Romantic
+        ['#260126', '#F2EEB3']  // Vintage Romantic
+      ];
+})();
