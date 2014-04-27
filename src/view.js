d910f453a7e7b202c5612739e6fd0b88f9cfad70
Jen Hamon <jhamon@gmail.com>
2014-04-27 01:41:13 -0700

    De-cruft view.

--- src/view.js
+++ src/view.js
@@ -1,33 +1,15 @@
 'use strict';
 
 (function () {
-  var GOL = window.GOL = (window.GOL || {});
+  var LIFE = window.LIFE = (window.LIFE || {});
 
-  var View = GOL.View = function () {
+  var View = LIFE.View = function () {
     this.canvas = document.getElementById('mycanvas');
     this.ctx = this.canvas.getContext('2d');
     this.configure();
   };
 
-  View.prototype.palettes = [
-    ['#44A437', '#081017'], // my original
-    ['#081017', '#44A437'], // my original
-    // Nice palettes from colourlovers.com:
-    ['#003A54', '#325D6F'], // Dawn Watch
-    ['#325D6F', '#003A54'], // Dawn Watch
-    ['#FFF700', '#004E72'], // The Last Summer
-    ['#004E72', '#FFF700'], // The Last Summer
-    ['#80B31A', '#082A3A'], // Phone Never Answer
-    ['#082A3A', '#80B31A'], // Phone Never Answer
-    ['#1CB4FA', '#FF0022'], // Friendship
-    ['#FF0022', '#1CB4FA'], // Friendship
-    ['#242004', '#009E77'], // Clementine Gozmin
-    ['#009E77', '#242004'], // Clementine Gozmin
-    ['#FF3434', '#C8D319'], // Sad
-    ['#C8D319', '#FF3434'], // Sad
-    ['#6B07C7', '#D3D0D6'], // Planet Industry
-    ['#D3D0D6', '#6B07C7']  // Planet Industry
-  ];
+  View.prototype.palettes = LIFE.palettes;
 
   View.prototype.configure = function () {
     // Resize canvas element to fix screen.
@@ -39,10 +21,16 @@
     this.calculateGridSize();
     this.setupListeners();
 
-    // Select a palette.
+    // Select a palette and fill the background color.
     this.palette = this.palettes[(Math.random() * this.palettes.length | 0)];
-
+    this.clearView();
   };
+
+  View.prototype.clearView = function () {
+    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    this.ctx.fillStyle = this.palette[1];
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 
   View.prototype.calculatePixelSize = function () {
     var pixelArea = window.innerWidth * window.innerHeight;
@@ -75,12 +63,8 @@
         window.open(v.canvas.toDataURL());
     });
 
-    $('#pause').click(function () { 
-      gameWorker.postMessage(Messages.pause);
-    });
-
-    $('#resume').click(function () { 
-      gameWorker.postMessage(Messages.resume);
+    $('#refresh').click(function () { 
+      location.reload();
     });
 
     $('#seed_ratio').mouseup(this.processForm.bind(this));
@@ -88,16 +72,13 @@
     $('#size').mouseup(this.processForm.bind(this));
   };
 
-  View.prototype.drawPixel = function (x, y, value) {
+  View.prototype.drawPixel = function (x, y, alive) {
     var pixelX = this.dx * x;
     var pixelY = this.dx * y;
-    this.ctx.fillStyle = value ? this.palette[0] : this.palette[1];
+    this.ctx.fillStyle = alive ? this.palette[0] : this.palette[1];
     this.ctx.fillRect(pixelX, pixelY, this.pixelSize, this.pixelSize);
   };
 
-  View.prototype.clearView = function () {
-    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  }
 
   View.prototype.drawPixelPayload = function (payload) {
     payload.forEach( function (pixel) {
@@ -128,28 +109,9 @@
         }
     },
 
-    reset: {
-      'command':'reset',
-      'commandParams': 
-        {
-          'x': v.gridWidth, 
-          'y': v.gridHeight,
-          'interval': 50,
-          'seed': 0.3
-        }
-     },
-
      modify: {
       'command':'modify',
       'commandParams': {}
-     },
-
-     pause: {
-      'command':'pause'
-     },
-
-     resume: {
-      'command':'resume'
      }
    }; // end Messages
 
@@ -158,7 +120,7 @@
     status = e.data.status;
     payload = e.data.payload;
     
-    if (status !== undefined) console.log(status);
+    if (status !== undefined) console.log('Worker says: ' + status);
     if (payload !== undefined) v.drawPixelPayload(payload);
   }, false);
 
